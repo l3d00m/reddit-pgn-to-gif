5f534ace0c391cb7b6e7d35534b87efa150546a2
Thomas <substantialimpulse@protonmail.com>
2020-06-13 21:05:18 +0200

    Fix missing last frame; show last frame longer

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Reddit: PGN to gif converter
 
-*Converts PGN in `[pgn][/pgn]` tags from http://reddit.com/r/chess into gifs hosted on imgur and to a [lichess analysis board](https://lichess.org/analysis). It comments the links as a reply on reddit.*
+*Converts PGN in `[pgn][/pgn]` tags from http://reddit.com/r/chess (among other subreddits) into gifs hosted on imgur and to a [lichess analysis board](https://lichess.org/analysis). It comments the links as a reply on reddit.*
 
 * Supports multiple PGN tags in a single post or comment
 * Uses [`python-chess`](https://github.com/niklasf/python-chess) which has a lax PGN parsing
--- pgn2gif.py
+++ pgn2gif.py
@@ -31,7 +31,6 @@ def convert_to_lila_gif_req(game):
         if i > 1000:
             print("Too many moves")
             break
-
         frame = {}
         if last_move is not False:
             frame["lastMove"] = last_move
@@ -41,6 +40,12 @@ def convert_to_lila_gif_req(game):
 
         board.push(move)
         frames.append(frame)
+    # Append last frame manually with longer delay
+    frame["lastMove"] = last_move
+    frame["fen"] = board.fen()
+    frame["check"] = board.is_check()
+    frame["delay"] = 500
+    frames.append(frame)
 
     req_body = {}
     req_body["white"] = game.headers["White"]
@@ -90,7 +95,7 @@ def upload_to_imgur():
         im.refresh_access_token()
         gif_slow = im.upload_image("slow.gif", title="Slow speed")
         gif_fast = im.upload_image("fast.gif", title="Fast speed")
-        album = im.create_album("/r/chess PGN gifs", images=[gif_fast.id, gif_slow.id])
+        album = im.create_album("Reddit PGNs converted to GIFs in fast & slow speed", images=[gif_fast.id, gif_slow.id])
         return album.link
     except Exception as e:
         if config.DEBUG:
@@ -100,5 +105,6 @@ def upload_to_imgur():
 
 
 if __name__ == "__main__":
-    convert_pgn_to_gif(
+    url = convert_pgn_to_gif(
         "[Event \"Rated Bullet game\"]\r\n[StartFlipped \"1\"]\r\n[Date \"2020.04.21\"]\r\n[Round \"-\"]\r\n[Black \"someone\"]\r\n[Result \"0-1\"]\r\n[UTCDate \"2020.04.21\"]\r\n[UTCTime \"08:39:28\"]\r\n[WhiteElo \"1411\"]\r\n[BlackElo \"1436\"]\r\n[WhiteRatingDiff \"-10\"]\r\n[BlackRatingDiff \"+6\"]\r\n[Variant \"Standard\"]\r\n[TimeControl \"60+0\"]\r\n[ECO \"B45\"]\r\n[Opening \"Sicilian Defense: Paulsen Variation, Normal Variation\"]\r\n[Termination \"Time forfeit\"]\r\n[Annotator \"lichess.org\"]\r\n\r\n1. e4 e6 2. Nf3 c5 3. Nc3 Nc6 4. d4 cxd4 5. Nxd4 { B45 Sicilian Defense: Paulsen Variation, Normal Variation } g6 6. Bd3 Bg7 7. Be3 Nxd4 8. Bxd4 Bxd4 9. Bb5 Bg7 10. Qd2 Ne7 11. O-O-O O-O 12. Bxd7 Bxd7 13. Qxd7 Qxd7 14. Rxd7 Nc6 15. Rxb7 Na5 16. Rb5 Nc4 17. Rc5 Bxc3 18. Rxc4 Bg7 19. Rd1 Rfc8 20. Rxc8+ Rxc8 21. f4 e5 22. f5 gxf5 23. exf5 Bh6+ 24. Kb1 Kg7 25. h4 Kf6 26. g4 Bf4 27. g5+ Kg7 28. f6+ Kg6 29. Rf1 Rc4 30. Re1 Rd4 31. c3 Rd2 { Black wins on time. } 0-1")
+    print(url)
